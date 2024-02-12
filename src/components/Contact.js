731ec6a1c0c3e1fb5cf90238705b95d2e71630c5
Ruby <jeromeeatspants@gmail.com>
2024-02-12 19:00:50 +0400

    fixed more stuff

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,8 +6,8 @@ import logo from '../images/ravira.png'
 export default function Contact() {
   return (
     <div className='contact-div' id="contact">
-        <a href="https://www.facebook.com/profile.php?id=100064539442989" target='_blank'><img src={logo}></img></a>
-        <p>571 33 35 59 Whatsapp/Viber</p>
+        <a href="https://www.facebook.com/profile.php?id=100064539442989" target='_blank'><img className="contact-image" src={logo}></img></a>
+        <p className='contact-text2'>571 33 35 59 Whatsapp/Viber</p>
     </div>
   )
 }
